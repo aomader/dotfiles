63aa602b071108ca0aa39fd111fdd65c556512f0
Oliver Mader <dotb52@gmail.com>
2010-07-13 20:15:38 +0200

    Reworked my xmonad setup which still lacks good keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,11 @@
-import System.Directory
-
 import XMonad
 import XMonad.Actions.CopyWindow
+import XMonad.Actions.DynamicWorkspaces as DW
 import XMonad.Actions.GridSelect
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Gaps
@@ -16,6 +16,7 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.WorkspaceDir as WD
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 
@@ -32,8 +33,9 @@ myFocusedColor = "#57666a"
 myCurrentColor = "#cd5c5c"
 myEmptyColor = "#4c4c4c"
 myHiddenColor = "#dddddd"
+myLayoutColor = "#666666"
+myUrgentColor = "#2b9ac8"
 myIcon name = myHome ++ "/.xmonad/icons/" ++ name ++ ".xbm"
-myLayout name = myIcon "layout-" ++ name
 
 myWorkspaces = ["main", "web", "dev", "misc"]
 myTerminal = "urxvt"
@@ -42,12 +44,18 @@ myModMask = mod4Mask
 
 myStartupHook = setWMName "LG3D"
 
-myLayoutHook = smartBorders . layoutHints . avoidStruts . gaps [(U, 3), (D, 3), (R, 3), (L, 3)] . spacing 3 $
-                 named "grid" Grid ||| named "tall" (Tall 1 (3/100) (1/2)) ||| named "full" Full
+myLayoutHook = onWorkspace "dev" (tall ||| grid) $
+             grid ||| tall ||| full
+             where myNamed n l = named n $ layoutHints . avoidStruts . gaps [(U, 3), (D, 3), (R, 3), (L, 3)] . spacing 3 $ l
+                   grid = myNamed "grid" Grid
+                   tall = myNamed "tall" (Tall 1 (3/100) (1/2))
+                   full = myNamed "full" Full
 
 myManageHook = composeAll $
                 [ className =? "MPlayer" --> doFloat <+> doF copyToAll
                 , className =? "Firefox" --> doShift "web"
+                , ((className =? "Firefox") <&&> (resource =? "Download")) --> doFloat <+> doShift "misc"
+                , ((className =? "Firefox") <&&> (resource =? "DAT")) --> doFloat <+> doShift "misc"
                 , resource =? "wicd-client.py" --> doFloat
                 , resource =? "DTA" --> doFloat <+> doShift "misc"
                 , className =? "org-igoweb-cgoban-CGoban" --> doFloat
@@ -66,6 +74,11 @@ myKeys conf = mkKeymap conf $
                , ("M-.", sendMessage Expand)
                , ("M-l", goToSelected defaultGSConfig)
                , ("M-<Space>", sendMessage NextLayout)
+               , ("<XF86MonBrightnessDown>", spawn "~/test.sh brightness")
+               , ("<XF86MonBrightnessUp>", spawn "~/test.sh brightness")
+               , ("<XF86AudioMute>", spawn "amixer set Master toggle; ~/test.sh audio")
+               , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%-; ~/test.sh audio")
+               , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5%+; ~/test.sh audio")
                ]
                ++
                [ (m ++ k, f i)
@@ -78,23 +91,28 @@ myKeys conf = mkKeymap conf $
 
 
 myStatusBar = "dzen2 -x 0 -y 0 -h 16 -w 640 -ta l -fn " ++ myFont ++ " -bg \"" ++ myBgColor ++ "\" -fg \"" ++ myFgColor ++ "\""
-myResourceBar = "conky -c ~/.xmonad/conky.conf | dzen2 -x 640 -y 0 -h 16 -w 640 -ta r -fn " ++ myFont ++ " -bg \"" ++ myBgColor ++ "\" -fg \"" ++ myFgColor ++ "\""
+myResourceBar = "MY_HOME=\"" ++ myHome ++ "\" conky -c ~/.xmonad/conky.conf | dzen2 -x 640 -y 0 -h 16 -w 640 -ta r -fn " ++ myFont ++ " -bg \"" ++ myBgColor ++ "\" -fg \"" ++ myFgColor ++ "\""
 myLogHook h = dynamicLogWithPP $ defaultPP
                                   { ppOutput = hPutStrLn h
-                                  , ppCurrent = wrap ("^bg(" ++ myBgColor ++ ")^i(" ++ myIcon "corner" ++ ")^fg(" ++ myCurrentColor ++ ")") "^fg()"
-                                  , ppHidden = wrap ("^i(" ++ myIcon "corner" ++ ")^fg(" ++ myHiddenColor ++ ")") "^fg()"
-                                  , ppHiddenNoWindows = wrap ("^i(" ++ myIcon "corner" ++ ")^fg(" ++ myEmptyColor ++ ")") "^fg()"
-                                  , ppUrgent = wrap ("^i(" ++ myIcon "corner" ++ ")^fg(" ++ myHiddenColor ++ ")") "^fg()"
-                                  , ppLayout = wrap "^i(" ")" . myLayout
+                                  , ppCurrent = corner . fg myCurrentColor
+                                  , ppHidden = corner . fg myHiddenColor
+                                  , ppHiddenNoWindows = corner . fg myEmptyColor
+                                  , ppUrgent = corner . fg myUrgentColor . dzenStrip
+                                  , ppLayout = fg myLayoutColor . layout
                                   , ppWsSep = "  "
-                                  , ppSep = "   "
-                                  , ppTitle = shorten 80
+                                  , ppSep = "     "
+                                  , ppTitle = (++) ("^fg(" ++ myFgColor ++ ")") . dzenEscape . shorten 100 . trim
                                   }
+            where fg c = dzenColor c ""
+                  icon n = "^i(" ++ (myIcon n) ++ ")"
+                  corner = (++) (icon "corner")
+                  layout n = icon ("layout-" ++ n)
 
 main = do
         status <- spawnPipe myStatusBar
         resource <- spawnPipe myResourceBar
-        xmonad $ defaultConfig
+        xmonad $ withUrgencyHook NoUrgencyHook
+               $ defaultConfig
                   { workspaces = myWorkspaces
                   , terminal = myTerminal
                   , borderWidth = myBorderWidth
